8ad13fc711e831b556a91da0558c0b9b7e5f7ab9
Sergey-Proskurnin <proskurnin@ua.fm>
2021-03-02 01:55:10 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Разбей задание на несколько подзадач:
 
 // Создание и рендер разметки по массиву данных и предоставленному шаблону. - Add
-// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения. - Add
 // Открытие модального окна по клику на элементе галереи.
 // Подмена значения атрибута src элемента img.lightbox__image.
 // Закрытие модального окна по клику на кнопку button[data-action="close-lightbox"].
@@ -23,8 +23,8 @@ import images from './gallery-items.js';
 // console.log(images);
 
 const imadgesListRef = document.querySelector('.js-gallery');
-// const modalRef = document.querySelector('.modal');
-// const modalImgRef = document.querySelector('.modal-img');
+const modalRef = document.querySelector('.js-lightbox');
+const modalImgRef = document.querySelector('.lightbox__image');
 // const formRef = document.querySelector(".form");
 // const inputRef = document.querySelector(".todo");
 // const listRef = document.querySelector(".todo-list");
@@ -53,6 +53,47 @@ const imadgesMarkup = images.reduce(
   },
   '',
 );
-console.log(imadgesMarkup);
+// console.log(imadgesMarkup);
 imadgesListRef.insertAdjacentHTML('afterbegin', imadgesMarkup);
+
+
+imadgesListRef.addEventListener('click', (event) => {
+  event.preventDefault();
+  console.dir(event.target.dataset.source);
+  console.dir(modalImgRef.src);
+  if (event.target.localName === 'img') {
+    modalRef.classList = 'is-open';
+    modalImgRef.src = event.target.dataset.source
+  }
+});
+
+
+
+
+
+
+// gelleryList.addEventListener("click", handleImadgeClick);
+
+// function handleImadgeClick(event) {
+//   // event.preventDefault();
+  
+//   const target = event.target;
+//   // console.log("event target: ", target); // посмотрите что тут
+
+//   // Проверяем тип узла, если не ссылка выходим из функции
+//   if (target.nodeName !== "IMG") return;
+
+//   setActiveImg(target);
+// }
+
+// // function setActiveImg(nextActiveLink) {
+// //   const currentActiveLink = nav.querySelector("a.active");
+
+// //   if (currentActiveLink) {
+// //     currentActiveLink.classList.remove("active");
+// //   }
+
+// //   nextActiveLink.classList.add("active");
+// // }
+
 
